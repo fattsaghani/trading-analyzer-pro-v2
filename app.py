07e0ece4f1bca520f1d771ae946ce718f5e0b268
fattsaghani <fatumaaghani@gmail.com>
2026-01-05 21:17:54 +0300

    Fix gunicorn import

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from app import create_app
 
 application = create_app()
+app = application  # alias for gunicorn
 
 if __name__ == "__main__":
     application.run(host="127.0.0.1", port=5000, debug=True)
